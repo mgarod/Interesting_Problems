99a4ea01769f04bab96fe2d5688575dc0e0f2303
Michael Garod <mgarod@gmail.com>
2016-12-05 13:32:13 -0500

    Add URL to deleteDuplicateList.cpp

--- Easy_Questions/deleteDuplicateList.cpp
+++ Easy_Questions/deleteDuplicateList.cpp
@@ -1,3 +1,13 @@
+/*
+https://codelab.interviewbit.com/problems/remduplnk/
+
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+For example,
+Given 1->1->2, return 1->2.
+Given 1->1->2->3->3, return 1->2->3.
+*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
